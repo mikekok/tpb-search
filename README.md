3f339af30882d0e293af4228d0a5b206c7550bf3
Mike Kokkolios <mikekokkolios@gmail.com>
2017-06-19 14:55:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# leetx
+A Node.js module to search for torrents at The Pirate Bay tracker.
+
+## Installation
+* `npm install tpb-search`
+
+## Usage
+```javascript
+const tpb = require('tpb-search')
+
+tpb.search(keyword, function(err, results) {
+  console.log(results) // returns name, seeders, leechers, url
+})
+```
+
+## Packages
+* [request](https://github.com/request/request)
+* [cheerio](https://github.com/cheeriojs/cheerio)
+
+## License
+This project is licensed under The MIT License (MIT). Which means that you can use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software. But you always need to state that xWeb is the original author of this template.
+
+Project is developed and maintained by [xWeb](https://xweb.gr/).
---
+++ index.js
@@ -0,0 +1,27 @@
+// Modules
+const request = require('request')
+const cheerio = require('cheerio')
+
+// 1337x.to base URL
+let tpbURL = 'https://thepiratebay.org'
+
+module.exports = {
+  search: function(keyword, cb) {
+    let torrents = []
+    var reqURL = tpbURL + '/search/' + keyword + '/0/99/0'
+    request(reqURL, function(err, res, body) {
+      var $ = cheerio.load(body)
+      $('table#searchResult tr').each(function(index, el) {
+        var torrent = {}
+        torrent.name = $(this).find('a.detLink').text()
+        torrent.seeders = $(this).find('td:nth-child(3)').text()
+        torrent.leechers = $(this).find('td:nth-child(4)').text()
+        torrent.url = tpbURL + $(this).find('a.detLink').attr('href')
+        if (torrent.name !== '') {
+          torrents.push(torrent)
+        }
+      })
+      return cb(null, torrents)
+    })
+  }
+}
